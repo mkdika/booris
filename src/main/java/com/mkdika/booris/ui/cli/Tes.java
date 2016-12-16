bdd223c96723ac98c4f58f858652f1e6bbfdb014
maikel <maikel@maikel-mint>
2016-12-16 14:12:44 +0700

    fixing

---
+++ src/main/java/com/mkdika/booris/ui/cli/Tes.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mkdika.booris.ui.cli;
+
+/**
+ *
+ * @author maikel
+ */
+public class Tes {
+    
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6,7,8,9,10};
+        for (int n : a) { 
+            System.out.println(n);
+        }
+    }
+    
+    
+    
+}
